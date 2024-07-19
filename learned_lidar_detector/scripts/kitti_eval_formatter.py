163e14e8ecd928e6e0a1825a993c4249ce29afc7
Adrian <adr.soch@outlook.com>
2024-07-19 10:06:18 -0400

    Update kitti prediction formatter

--- learned_lidar_detector/scripts/kitti_eval_formatter.py
+++ learned_lidar_detector/scripts/kitti_eval_formatter.py
@@ -4,14 +4,8 @@ Formats test results into the KITTI format for evalulation.
 # fmt: off
 # limit the number of cpus used by high performance libraries
 import os
-os.environ["OMP_NUM_THREADS"] = "1"
-os.environ["OPENBLAS_NUM_THREADS"] = "1"
-os.environ["MKL_NUM_THREADS"] = "1"
-os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
-os.environ["NUMEXPR_NUM_THREADS"] = "1"
 
 import argparse
-import cv2
 import glob
 from os import path
 import numpy as np
@@ -28,10 +22,11 @@ from lidar_2_bev import transform_pc
 
 
 class KittiEval():
-    def __init__(self, bev_path: str, method: str, out_path: str):
+    def __init__(self, bev_path: str, method: str, out_path: str, imgsz: int):
         self.method = method
         self.out_path = out_path
         self.bev_list = (get_files(bev_path, 'jpg'))
+        self.imgsz = imgsz
         assert len(self.bev_list) > 0, "No files found."
         print(f'Found {len(self.bev_list)} data samples.')
 
@@ -39,19 +34,27 @@ class KittiEval():
             self.out_path), "Output folder exists already"
         os.makedirs(self.out_path)
 
-    def get_model_output(self, model_path: str, conf=0.2, device='cuda:0', imgsz=608):
+    def get_model_output(self, model_path: str, conf=0.2, device='cuda:0'):
         model = YOLO(model_path, task='obb')
 
-        img_list = []
-        for idx in range(len(self.bev_list)):
-            bev_path = self.bev_list[idx]
-            img = cv2.imread(bev_path)
-            img_list.append(img)
+        batch_size = 1
+        path_list = self.bev_list[:]
+        while path_list:
 
-        results = model(img_list, device=device, imgsz=(
-            imgsz), conf=conf, verbose=False)
+            # Get batch of paths
+            batch = []
+            while path_list and len(batch) < batch_size:
+                batch.append(path_list.pop(0))
 
-        for idx, res in enumerate(results):
+            results = model(batch, device=device, imgsz=(
+                self.imgsz), conf=conf, verbose=True, half=False)
+
+            import time
+            time.sleep(0.02)
+            self.create_prediction_files(results)
+
+    def create_prediction_files(self, results):
+        for res in results:
             obb_results = res.obb.cpu().numpy()
             class_dict = res.names
 
@@ -67,7 +70,9 @@ class KittiEval():
                 else:
                     kitti_entries.append(None)
 
-            file_name = str(idx).zfill(6)
+            tail = os.path.split(res.path)[-1]
+            file_name = os.path.splitext(tail)[0]
+
             file_path = path.join(self.out_path, f'{file_name}.txt')
             self.write_predicted_labels(file_path, kitti_entries)
 
@@ -80,12 +85,12 @@ class KittiEval():
         """
         truncated = -1
         occluded = -1
-        alpha = -1.57
+        alpha = -10
         h = self.height_from_class(cls)
         w, l = lidar_det[2], lidar_det[3]
 
         lidar_frame_coord = [lidar_det[0],
-                             lidar_det[1], h/2 + cfg.boundary['minZ']]
+                             lidar_det[1], h/2 - 1.7]
         # 3D in camera coordinates
         [x, y, z] = transform_pc(
             np.array([lidar_frame_coord]), cfg.Tr_velo_to_cam).reshape(-1)
@@ -95,12 +100,14 @@ class KittiEval():
             lidar_frame_coord + [h, w, l, lidar_det[4]])
         image_3d_corners = self.project_to_image(
             np.array(lidar_3d_corners), cfg.P_velo_to_image).astype(int)
+
+        if image_3d_corners.shape[0] != 8:
+            return None
         min_x, max_x = min(image_3d_corners[:, 0]), max(image_3d_corners[:, 0])
         min_y, max_y = min(image_3d_corners[:, 1]), max(image_3d_corners[:, 1])
-        
+
         bbox = [min_x, min_y, max_x, max_y]
 
-        # self.project_to_image
         ry = lidar_det[4]
 
         if self.method == '2D':
@@ -110,7 +117,7 @@ class KittiEval():
             h = 0
 
         return (f'{cls} {truncated} {occluded} {alpha} {bbox[0]} '
-                f'{bbox[1]} {bbox[2]} {bbox[3]} {0} {w:.3f} {l:.3f} '
+                f'{bbox[1]} {bbox[2]} {bbox[3]} {h:.3f} {w:.3f} {l:.3f} '
                 f'{x:.3f} {y:.3f} {z:.3f} {ry:.3f} {conf:.3f}')
 
     def px_to_meters(self, xywhr: np.ndarray) -> np.ndarray:
@@ -125,13 +132,12 @@ class KittiEval():
         x, y, w, h, r = xywhr
 
         # Weird switcharoo for correct mapping, don't ask
-        r *= -1.0
         y = (y * cfg.DISCRETIZATION) + cfg.boundary['minX']
-        x = (x * cfg.DISCRETIZATION) + cfg.boundary['minY']
+        x = (x * cfg.DISCRETIZATION_y) + cfg.boundary['minY']
         x, y = y, x
 
-        w *= cfg.DISCRETIZATION
-        h *= cfg.DISCRETIZATION
+        w *= cfg.DISCRETIZATION_y
+        h *= cfg.DISCRETIZATION_y
 
         return (float(x), float(y), float(h), float(w), float(r))
 
@@ -161,14 +167,14 @@ class KittiEval():
         x4 = (l/2 * cos_yaw + w/2 * sin_yaw) + x
         y4 = (l/2 * sin_yaw - w/2 * cos_yaw) + y
 
-        return [[x1, y1, z/2 - h/2.0],
-                [x2, y2, z/2 - h/2.0],
-                [x3, y3, z/2 - h/2.0],
-                [x4, y4, z/2 - h/2.0],
-                [x1, y1, z/2 + h/2.0],
-                [x2, y2, z/2 + h/2.0],
-                [x3, y3, z/2 + h/2.0],
-                [x4, y4, z/2 + h/2.0]]
+        return [[x1, y1, z - h/2.0],
+                [x2, y2, z - h/2.0],
+                [x3, y3, z - h/2.0],
+                [x4, y4, z - h/2.0],
+                [x1, y1, z + h/2.0],
+                [x2, y2, z + h/2.0],
+                [x3, y3, z + h/2.0],
+                [x4, y4, z + h/2.0]]
 
     @staticmethod
     def project_to_image(points: np.ndarray, proj_mat: np.ndarray) -> np.ndarray:
@@ -176,21 +182,21 @@ class KittiEval():
         image_points = image_points[:, :2]/image_points[:, 2:]
 
         # Filter out-of-bounds points
-        image_width, image_height = 1242*1.25, 375*1.25  # KITTI image dimensions
-        valid_indices = np.logical_and.reduce((
-            image_points[:, 0] >= 0,
-            image_points[:, 0] < image_width,
-            image_points[:, 1] >= 0,
-            image_points[:, 1] < image_height
-        ))
-        image_points = image_points[valid_indices]
+        # image_width, image_height = 1242*1.2, 375*1.2  # KITTI image dimensions
+        # valid_indices = np.logical_and.reduce((
+        #     image_points[:, 0] >= 0,
+        #     image_points[:, 0] < image_width,
+        #     image_points[:, 1] >= 0,
+        #     image_points[:, 1] < image_height
+        # ))
+        # image_points = image_points[valid_indices]
 
         return image_points
 
     @staticmethod
     def height_from_class(cls: str) -> float:
         CLASS_NAME_TO_HEIGHT = {'Cyclist': 1.6,
-                                'Truck': 2.75, 'Car': 2.0, 'Pedestrian': 1.9}
+                                'Truck': 3.2, 'Car': 1.7, 'Pedestrian': 1.9}
         return CLASS_NAME_TO_HEIGHT[cls]
 
     def write_predicted_labels(self, file_path, data):
@@ -211,10 +217,10 @@ def get_files(path, ext: str) -> list:
 
 def main(args):
     kiti_eval_creator = KittiEval(
-        bev_path=args.bev_path, method=args.method, out_path=args.output)
+        bev_path=args.bev_path, method=args.method, out_path=args.output, imgsz=args.imgsz)
 
     kiti_eval_creator.get_model_output(
-        model_path='/home/adrian/dev/kitti/yolov83dBEV_n_608.pt')
+        model_path='/home/adrian/dev/kitti/yolov8n-obb-p2.pt')
 
 
 # check if the script is run directly and call the main function
@@ -224,11 +230,14 @@ if __name__ == "__main__":
     parser.add_argument(
         "-m", "--method", help="The evaluation method ['2D', 'BEV', '3D'].", default='BEV', type=str)
     parser.add_argument(
-        "-l", "--bev_path", help="The lidar BEV psuedo image path.", default='/home/adrian/dev/kitti/test_608/', type=str)
+        "-l", "--bev_path", help="The lidar BEV psuedo image path.", default='/home/adrian/dev/kitti/kitti_bev_last/images/val/', type=str)
     parser.add_argument(
-        "-o", "--output", help="The path where the results are saved.", default='/home/adrian/dev/kitti/bev_test_predictions', type=str)
+        "-o", "--output", help="The path where the results are saved.", default='/home/adrian/dev/kitti/bev_p21024_val_predictions/data', type=str)
+    parser.add_argument(
+        "--imgsz", help="The image size.", default=1024, type=int)
+    
     args = parser.parse_args()
 
-    if args.method not in ['2D', 'BEV', '3D']:
+    if args.method.upper() not in ['2D', 'BEV', '3D']:
         exit("Not a valid evaluation method, must be in ['2D', 'BEV', '3D']")
     main(args)
